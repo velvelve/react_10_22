aee450e39f24bb27c387a43483472bc281e13a9c
Sandroisu <sandroisu1411@gmail.com>
2022-11-06 10:01:34 +0300

    lesson done

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,14 @@
+
+import style from './Form.module.css'
+
+export const Form = () => {
+    const count = 1;
+    const name = 'geekbrains';
+    return <form className={style.form}>
+        <p>Count: {count}</p>
+        <p>Name: {name}</p>
+        <input type="text" />
+        <button type="button">click</button>
+    </form>
+}
+
