67d8b7c570a5a7ef86b050615d81e1b9a2ffc071
Sandroisu <sandroisu1411@gmail.com>
2022-12-27 17:39:32 +0300

    done firebase

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,8 +2,8 @@ import { FC } from 'react';
 import cat from '../assets/img/cat.jpeg';
 
 export const Main: FC = () => (
-    <>
-        <h2>Main page</h2>
-        <img src={cat} alt="cat" />
-    </>
+  <>
+    <h2>Main page</h2>
+    <img src={cat} alt="cat" />
+  </>
 );
